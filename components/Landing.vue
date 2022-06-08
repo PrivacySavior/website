2f5f5af71ec839d76eed0155b65e8ab0837e390f
Anthony Zhen <anthony20151128@gmail.com>
2022-06-08 15:58:22 -0400

    resize and 100 images

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="bg-secondary h-screen w-screen" ref="Main">
-      <video @canplay="video_loaded" src="https://res.cloudinary.com/privacysaviorllc/video/upload/v1654658856/videos/0001-0100_ar1jwi.mkv" muted autoplay loop class="absolute h-screen w-screen brightness-75 object-cover"></video>
+      <canvas ref="video" class="absolute h-full w-full brightness-75 object-cover"></canvas>
       <div class="relative top-64 flex justify-around items-center">
         <div>
           <LeftAppear
@@ -32,14 +32,44 @@
 <script>
 export default {
     name: "Landing",
+    data(){
+      return {
+        frameCount: 70,
+        images: [],
+        context: null,
+        frame: 0
+      }
+    },
     methods: {
         appear(){
             this.$refs.first_text.appear()
         },
         video_loaded(){
-          console.log("Video ready")
           this.$emit("loaded")
+        },
+        currentFrame(index){
+          return `https://res.cloudinary.com/privacysaviorllc/image/upload/v1654711303/product-split/${(index + 1).toString().padStart(4, '0')}.png`
+        },
+        render() {
+          this.context.clearRect(0, 0, this.$refs.video.width, this.$refs.video.height);
+          this.context.drawImage(this.images[this.frame], 0, 0); 
+        },
+        resize_canvas(){
+          this.$refs.video.height = window.outerHeight
+          this.$refs.video.width = window.outerWidth
         }
+    },
+    mounted(){
+      this.resize_canvas()
+      window.addEventListener("resize", this.resize_canvas)
+      this.video_loaded()
+      this.context = this.$refs.video.getContext("2d")
+      for (let i = 0; i < this.frameCount; i++) {
+        const img = new Image();
+        img.src = this.currentFrame(i);
+        this.images.push(img);
+      }
+      this.images[0].onload = this.render;
     }
 }
 </script>
